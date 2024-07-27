b65752ebd1949e00b41e6b9c9eadf3dfcf08712d
IsaiasCuvula <bersyteinfo@gmail.com>
2024-07-28 02:16:26 +0300

    initial

---
+++ src/main/java/com/beryste/eventz/EventzApplication.java
@@ -0,0 +1,13 @@
+package com.beryste.eventz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beryste/eventz/EventzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beryste.eventz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
