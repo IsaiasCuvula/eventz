3f77acac7fbe0dda28ccb7e7859859f3e7361cb8
IsaiasCuvula <bersyteinfo@gmail.com>
2024-08-06 11:49:42 +0300

    SpringBootTest

--- src/test/java/com/beryste/eventz/EventzApplicationTests.java
+++ src/test/java/com/beryste/eventz/EventzApplicationTests.java
@@ -1,9 +1,10 @@
 package com.beryste.eventz;
 
+import com.bersyte.eventz.EventzApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EventzApplication.class)
 class EventzApplicationTests {
 
 	@Test
