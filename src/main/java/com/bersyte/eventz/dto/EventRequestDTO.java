f45cbc0c78d41cbc017157f7e5b690bce912f268
IsaiasCuvula <bersyteinfo@gmail.com>
2024-08-06 03:34:24 +0300

    NotBlank

--- src/main/java/com/bersyte/eventz/dto/EventRequestDTO.java
+++ src/main/java/com/bersyte/eventz/dto/EventRequestDTO.java
@@ -3,10 +3,13 @@ import java.util.Date;
 import jakarta.validation.constraints.*;
 
 public record EventRequestDTO(
+        @NotBlank
         @NotNull(message = "title is required")
         String title,
+        @NotBlank
         @NotNull(message = "description is required")
         String description,
+        @NotBlank
         @NotNull(message = "location is required")
         String location,
         @NotNull(message = "date is required")
