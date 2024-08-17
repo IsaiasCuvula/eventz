68add19e7ffbc9762a2d63f92cc8162375885b01
IsaiasCuvula <bersyteinfo@gmail.com>
2024-08-17 12:59:01 +0300

    check if user is already registered

--- src/main/java/com/bersyte/eventz/event_registration/RegistrationRepository.java
+++ src/main/java/com/bersyte/eventz/event_registration/RegistrationRepository.java
@@ -1,8 +1,15 @@
 package com.bersyte.eventz.event_registration;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+
+  @Query(
+          "select count(r) > 0 from  Registration r where r.user.id = :userId " +
+                  " and r.event.id = :eventId"
+  )
+  boolean existsByUserIdAndEventId(Long userId, Long eventId);
 }
