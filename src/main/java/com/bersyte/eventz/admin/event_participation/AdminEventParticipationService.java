ba1affb02d321184b970d0179c042b7970d79438
IsaiasCuvula <bersyteinfo@gmail.com>
2024-08-19 12:13:23 +0300

    admin can add and cancel registration

--- src/main/java/com/bersyte/eventz/admin/event_participation/AdminEventParticipationService.java
+++ src/main/java/com/bersyte/eventz/admin/event_participation/AdminEventParticipationService.java
@@ -4,27 +4,32 @@ import com.bersyte.eventz.common.AppUser;
 import com.bersyte.eventz.common.EventCommonService;
 import com.bersyte.eventz.common.EventParticipationCommonService;
 import com.bersyte.eventz.common.UserCommonService;
-import com.bersyte.eventz.event_participation.EventParticipationResponseDTO;
+import com.bersyte.eventz.event_participation.*;
 import com.bersyte.eventz.events.Event;
 import com.bersyte.eventz.exceptions.EventRegistrationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
 public class AdminEventParticipationService {
     final private EventParticipationCommonService eventParticipationCommonService;
+    final private EventParticipationRepository eventParticipationRepository;
     final private EventCommonService eventCommonService;
     final private UserCommonService userCommonService;
+
 
 
     public EventParticipationResponseDTO registerUserToEvent(
             Long participantId, Long eventId
     ) {
         try {
-            AppUser participant = userCommonService.getUserById(participantId);
-            Event event = eventCommonService.findEventById(eventId);
+            AppUser participant = getUserById(participantId);
+            Event event = getEventById(eventId);
             return eventParticipationCommonService.registerUserToEvent(
                     participant, event
             );
@@ -35,8 +40,44 @@ public class AdminEventParticipationService {
         }
     }
 
-    //TODO-DELETE REGISTRATION
-    public EventParticipationResponseDTO removeParticipantFromEvent(Long participantId, Long eventId) {
-        return null;
+    public EventParticipationResponseDTO removeParticipantFromEvent(
+            Long participantId, Long eventId
+    ) {
+        try {
+            AppUser participant = getUserById(participantId);
+            Event event = getEventById(eventId);
+
+            Optional<EventParticipation> registration =
+                    eventParticipationRepository.findByUserIdAndEventId(
+                            participant.getId(), event.getId()
+                    );
+
+            if (registration.isEmpty()) {
+                throw new EventRegistrationException("Event with id " + eventId + " does not exist");
+            }
+
+            EventParticipation existingRegistration = registration.get();
+
+            if (existingRegistration.getStatus() == ParticipationStatus.CANCELED) {
+                throw new EventRegistrationException("Event with id " + eventId + " has already been canceled");
+            }
+
+            existingRegistration.setStatus(ParticipationStatus.CANCELED);
+            existingRegistration.setUpdateAt(new Date());
+            final EventParticipation result = eventParticipationRepository.save(existingRegistration);
+            return EventParticipationMapper.toResponseDTO(result);
+        } catch (EventRegistrationException e) {
+            throw new EventRegistrationException(
+                    "Failed to cancel registration from the event - " + e.getLocalizedMessage()
+            );
+        }
+    }
+
+    private AppUser getUserById(Long userId) {
+        return userCommonService.getUserById(userId);
+    }
+
+    private Event getEventById(Long eventId) {
+        return eventCommonService.findEventById(eventId);
     }
 }
