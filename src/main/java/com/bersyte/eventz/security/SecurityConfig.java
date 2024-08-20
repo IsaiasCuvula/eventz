a52c84a0b92f456e352486d97191b14598f24f9c
IsaiasCuvula <bersyteinfo@gmail.com>
2024-08-21 02:42:49 +0300

    added corsConfigurationSource

--- src/main/java/com/bersyte/eventz/security/SecurityConfig.java
+++ src/main/java/com/bersyte/eventz/security/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
@@ -58,6 +63,25 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(
+                List.of(
+                        "https://eventz.com", "http://localhost:8080"
+                )
+        );
+        config.setAllowedMethods(
+                List.of("GET", "POST", "PUT", "DELETE")
+        );
+        config.setAllowedHeaders(
+                List.of("Authorization", "Content-Type")
+        );
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
