655f4fa8122632d975a1e422b5ad485e05097b0b
IsaiasCuvula <bersyteinfo@gmail.com>
2024-08-27 22:27:20 +0300

    EventRepository Tested

--- src/test/java/com/bersyte/eventz/events/EventRepositoryTest.java
+++ src/test/java/com/bersyte/eventz/events/EventRepositoryTest.java
@@ -4,7 +4,7 @@ import com.bersyte.eventz.common.AppUser;
 import com.bersyte.eventz.common.UserRole;
 import com.bersyte.eventz.event_participation.EventParticipation;
 import com.bersyte.eventz.users.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -23,19 +23,129 @@ class EventRepositoryTest {
 
     @Autowired
     private EventRepository eventRepository;
-
     @Autowired
     private UserRepository userRepository;
 
-    @BeforeEach
-    void setUp() {
+    @AfterEach
+    void tearDown() {
+        eventRepository.deleteAll ();
+    }
+
+
+    @Test
+    void shouldReturnAllUpcomingEvents() {
+        // Given
+        saveEventsForTest ();
+        Pageable pageable = PageRequest.of (0, 10);
+
+        //When
+        Page<Event> eventsPerPage = eventRepository.getUpcomingEvents (
+                new Date (), pageable
+        );
+
+        //Act - Assert
+        assertEquals (1, eventsPerPage.getTotalElements ());
+    }
+
+    @Test
+    void shouldReturnAllEventsByGivenDate() {
+        // Given
+        saveEventsForTest ();
+        Date date = new Date (1723276144000L);
+        Pageable pageable = PageRequest.of (0, 10);
+
+        //When
+        Page<Event> eventsPerPage = eventRepository.getEventsByDate (
+                date, pageable
+        );
+
+        //Act - Assert
+        assertEquals (1, eventsPerPage.getTotalElements ());
+    }
+
+    @Test
+    void shouldReturnAllEventsByGivenTitleAndLocation() {
+        // Given
+        saveEventsForTest ();
+        Pageable pageable = PageRequest.of (0, 10);
+
+        //When
+        Page<Event> eventsPerPage = eventRepository.filterEventsByTitleAndLocation (
+                "New Launch Event", "Luanda", pageable
+        );
+
+        //Act - Assert
+        assertEquals (1, eventsPerPage.getTotalElements ());
+        assertEquals (
+                "Luanda City Center",
+                eventsPerPage
+                        .get ()
+                        .toList ()
+                        .getFirst ()
+                        .getLocation ()
+        );
+
+        assertEquals (
+                "New Launch Event",
+                eventsPerPage
+                        .get ()
+                        .toList ()
+                        .getFirst ()
+                        .getTitle ()
+        );
+    }
+
+
+    @Test
+    void shouldReturnNoEventsIfGivenTitleAndLocationDoesNotExists() {
+        // Given
+        Pageable pageable = PageRequest.of (0, 10);
+
+        //When
+        Page<Event> eventsPerPage = eventRepository.filterEventsByTitleAndLocation (
+                "Naval Academy", "", pageable
+        );
+
+        //Act - Assert
+        assertEquals (0, eventsPerPage.getTotalElements ());
+    }
+
+    private void saveEventsForTest() {
 
         List<EventParticipation> participants = List.of ();
+        AppUser organizer = getOrganizerForTest ();
+
+        //past event
+        Event event = new Event ();
+        event.setId (1L);
+        event.setOrganizer (organizer);
+        event.setTitle ("Product Launch Event");
+        event.setDescription ("Launch of new product line X");
+        event.setLocation ("City Convention Center");
+        event.setDate (new Date (1723276144000L));
+        event.setCreatedAt (new Date ());
+        event.setParticipants (participants);
+
+        //upcoming event
+        Event event2 = new Event ();
+        event2.setId (2L);
+        event2.setOrganizer (organizer);
+        event2.setTitle ("New Launch Event");
+        event2.setDescription ("Launch of X line in Luanda");
+        event2.setLocation ("Luanda City Center");
+        event2.setDate (new Date (System.currentTimeMillis () + 604800000));
+        event2.setCreatedAt (new Date ());
+        event2.setParticipants (participants);
+
+        eventRepository.saveAll (List.of (event, event2));
+
+    }
+
+
+    private AppUser getOrganizerForTest() {
         List<EventParticipation> registrations = List.of ();
         List<Event> events = List.of ();
-
-
-        AppUser organizer = new AppUser (
+        AppUser user = new AppUser (
                 1L,
                 "isaias@gmail.com",
                 "123456",
@@ -51,48 +161,8 @@ class EventRepositoryTest {
                 true
         );
 
-        userRepository.save (organizer);
-
-        //past event
-        Event event = new Event (
-                1L,
-                "Product Launch Event",
-                "Launch of new product line X",
-                "City Convention Center",
-                new Date (System.currentTimeMillis () - 604800000),
-                new Date (),
-                organizer,
-                participants
-        );
-
-        //upcoming event
-        Event event2 = new Event (
-                2L,
-                "New Launch Event",
-                "Launch of X line",
-                "Luanda",
-                new Date (System.currentTimeMillis () + 604800000),
-                new Date (),
-                organizer,
-                participants
-        );
-
-        eventRepository.saveAll (List.of (event, event2));
+        return userRepository.save (user);
     }
 
-
-    @Test
-    void shouldReturnAllUpcomingEvents() {
-        // Given
-        Pageable pageable = PageRequest.of (0, 10);
-
-        //When
-        Page<Event> eventsPerPage = eventRepository.getUpcomingEvents (
-                new Date (), pageable
-        );
-
-        //Act - Assert
-        assertEquals (1, eventsPerPage.getTotalElements ());
-    }
 
 }
