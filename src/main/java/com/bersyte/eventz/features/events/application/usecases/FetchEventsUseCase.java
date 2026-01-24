1fcdd5b57942a918613aeaa4542a7c63a254446d
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-24 11:23:15 +0200

    feat: implemented FetchEventsUseCase

---
+++ src/main/java/com/bersyte/eventz/features/events/application/usecases/FetchEventsUseCase.java
@@ -0,0 +1,35 @@
+package com.bersyte.eventz.features.events.application.usecases;
+
+import com.bersyte.eventz.common.application.usecases.UseCase;
+import com.bersyte.eventz.common.domain.PagedResult;
+import com.bersyte.eventz.common.domain.Pagination;
+import com.bersyte.eventz.features.events.application.dtos.EventResponse;
+import com.bersyte.eventz.features.events.application.mappers.EventMapper;
+import com.bersyte.eventz.features.events.domain.model.Event;
+import com.bersyte.eventz.features.events.domain.repository.EventRepository;
+
+import java.util.List;
+
+public class FetchEventsUseCase implements UseCase<Pagination, PagedResult<EventResponse>> {
+    private final EventRepository repository;
+    private final EventMapper mapper;
+    
+    public FetchEventsUseCase(EventRepository repository, EventMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+    @Override
+    public PagedResult<EventResponse> execute(Pagination pagination) {
+        PagedResult<Event> pagedResult = repository.fetchEvents(pagination);
+        List<EventResponse> responses = pagedResult.data().stream().map(
+                 mapper::toResponse
+        ).toList();
+        
+        return new PagedResult<>(
+                responses,
+                pagedResult.totalElements(),
+                pagedResult.totalPages(),
+                pagedResult.isLast()
+        );
+    }
+}
