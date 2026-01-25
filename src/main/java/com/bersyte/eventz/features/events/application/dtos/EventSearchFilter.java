c72f079b79f20f0e4973e99403bb6cfe8d2cccd0
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-25 14:54:26 +0200

    add: search events filter

---
+++ src/main/java/com/bersyte/eventz/features/events/application/dtos/EventSearchFilter.java
@@ -0,0 +1,14 @@
+package com.bersyte.eventz.features.events.application.dtos;
+
+import com.bersyte.eventz.common.domain.Pagination;
+
+import java.time.LocalDateTime;
+
+public record EventSearchFilter(
+        String title,
+        String location,
+        String category,
+        LocalDateTime startDate,
+        Pagination pagination
+) {
+}
