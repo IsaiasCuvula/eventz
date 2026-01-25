37e245652ccf05fa30754528680d511110df1557
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-25 14:53:39 +0200

    add: implemented Fetch Upcoming Events Use Case

---
+++ src/main/java/com/bersyte/eventz/features/events/application/usecases/FetchUpcomingEventsUseCase.java
@@ -0,0 +1,26 @@
+package com.bersyte.eventz.features.events.application.usecases;
+
+import com.bersyte.eventz.common.application.usecases.UseCase;
+import com.bersyte.eventz.common.domain.PagedResult;
+import com.bersyte.eventz.common.domain.Pagination;
+import com.bersyte.eventz.features.events.application.dtos.EventResponse;
+import com.bersyte.eventz.features.events.application.mappers.EventMapper;
+import com.bersyte.eventz.features.events.domain.model.Event;
+import com.bersyte.eventz.features.events.domain.repository.EventRepository;
+
+
+public class FetchUpcomingEventsUseCase implements UseCase<Pagination, PagedResult<EventResponse>> {
+    private final EventRepository repository;
+    private final EventMapper mapper;
+    
+    public FetchUpcomingEventsUseCase(EventRepository repository, EventMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+    
+    @Override
+    public PagedResult<EventResponse> execute(Pagination pagination) {
+        PagedResult<Event> result = repository.fetchUpcomingEvents(pagination);
+        return mapper.toPagedResponse(result);
+    }
+}
