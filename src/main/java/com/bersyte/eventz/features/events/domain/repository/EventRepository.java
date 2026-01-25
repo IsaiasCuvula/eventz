c8e02e5570e4550f7192aa76fa172e73f74b9fc9
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-25 14:54:05 +0200

    fix: renamed filter events

--- src/main/java/com/bersyte/eventz/features/events/domain/repository/EventRepository.java
+++ src/main/java/com/bersyte/eventz/features/events/domain/repository/EventRepository.java
@@ -3,22 +3,20 @@ package com.bersyte.eventz.features.events.domain.repository;
 import com.bersyte.eventz.common.domain.PagedResult;
 import com.bersyte.eventz.common.domain.Pagination;
 import com.bersyte.eventz.features.events.domain.model.Event;
-import java.util.Date;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface EventRepository {
     Event createEvent(Event event);
     Event updateEvent(Event event);
     void deleteEvent(String id);
-    Optional<Event> getEventById(String id);
     boolean existsById(String id);
-    
+    Optional<Event> getEventById(String id);
     PagedResult<Event> fetchEvents(Pagination pagination);
-   
-    PagedResult<Event> getEventsByDate(Pagination pagination, Date date);
-    PagedResult<Event> fetchUpcomingEvents(Date date, Pagination pagination);
-    PagedResult<Event> filterEventsByTitleAndLocation(
-            String title, String location, Pagination pagination);
+    PagedResult<Event> fetchEventsByDate(Pagination pagination, LocalDateTime date);
+    PagedResult<Event> fetchUpcomingEvents(Pagination pagination);
+    PagedResult<Event> filterEvents(String title, String location, Pagination pagination);
 }
 
 
