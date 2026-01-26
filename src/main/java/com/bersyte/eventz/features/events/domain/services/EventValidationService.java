dd16e1f8e4ce1ef43a066c1e2c2b344bf1ffd3a0
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-26 08:41:29 +0200

    added: EventValidationService

---
+++ src/main/java/com/bersyte/eventz/features/events/domain/services/EventValidationService.java
@@ -0,0 +1,19 @@
+package com.bersyte.eventz.features.events.domain.services;
+
+import com.bersyte.eventz.common.domain.exceptions.ResourceNotFoundException;
+import com.bersyte.eventz.features.events.domain.model.Event;
+import com.bersyte.eventz.features.events.domain.repository.EventRepository;
+
+public class EventValidationService {
+    private final EventRepository eventRepository;
+    
+    public EventValidationService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+    
+    public Event getValidEventById(String eventId){
+        return eventRepository.findEventById(eventId)
+                       .orElseThrow(()-> new ResourceNotFoundException("Event", eventId));
+        
+    }
+}
