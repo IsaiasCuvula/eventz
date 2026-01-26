996cff33dcd3aa04e4e6f3dee7c16a409228aecd
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-26 08:39:50 +0200

    fix: validate event & user before save delete

--- src/main/java/com/bersyte/eventz/features/events/application/usecases/DeleteEventUseCase.java
+++ src/main/java/com/bersyte/eventz/features/events/application/usecases/DeleteEventUseCase.java
@@ -1,21 +1,39 @@
 package com.bersyte.eventz.features.events.application.usecases;
 
 import com.bersyte.eventz.common.application.usecases.VoidUseCase;
-import com.bersyte.eventz.common.domain.exceptions.ResourceNotFoundException;
+import com.bersyte.eventz.common.domain.exceptions.UnauthorizedException;
+import com.bersyte.eventz.features.events.application.dtos.DeleteEventInput;
+import com.bersyte.eventz.features.events.domain.model.Event;
 import com.bersyte.eventz.features.events.domain.repository.EventRepository;
+import com.bersyte.eventz.features.events.domain.services.EventValidationService;
+import com.bersyte.eventz.features.users.domain.model.AppUser;
+import com.bersyte.eventz.features.users.domain.services.UserValidationService;
 
-public class DeleteEventUseCase implements VoidUseCase<String> {
+public class DeleteEventUseCase implements VoidUseCase<DeleteEventInput> {
     private final EventRepository repository;
+    private  final UserValidationService userValidationService;
+    private final EventValidationService eventValidationService;
     
-    public DeleteEventUseCase(EventRepository repository) {
+    public DeleteEventUseCase(EventRepository repository, UserValidationService userValidationService, EventValidationService eventValidationService) {
         this.repository = repository;
+        this.userValidationService = userValidationService;
+        this.eventValidationService = eventValidationService;
     }
     
     @Override
-    public void execute(String id) {
-        if(!repository.existsById(id)){
-            throw new ResourceNotFoundException("Event", id);
+    public void execute(DeleteEventInput input) {
+        String userEmail = input.userEmail();
+        String eventId = input.eventId();
+        
+        AppUser user = userValidationService.validateAndGetUser(userEmail);
+        if(!user.canManageEvents()){
+            throw new UnauthorizedException("Insufficient permissions to create events");
         }
-        repository.deleteEvent(id);
+        
+        Event event = eventValidationService.getValidEventById(eventId);
+        if(!user.isAdmin() && !event.isOwnedBy(user)){
+            throw new UnauthorizedException("You don't own this event");
+        }
+        repository.deleteEvent(eventId);
     }
 }
