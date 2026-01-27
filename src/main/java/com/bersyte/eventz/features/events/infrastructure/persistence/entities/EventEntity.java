c8a796359c0ab51611e5d4447414ea7b7568e096
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-27 15:46:51 +0200

    add: added participants count field

--- src/main/java/com/bersyte/eventz/features/events/infrastructure/persistence/entities/EventEntity.java
+++ src/main/java/com/bersyte/eventz/features/events/infrastructure/persistence/entities/EventEntity.java
@@ -1,12 +1,15 @@
-package com.bersyte.eventz.features.events;
+package com.bersyte.eventz.features.events.infrastructure.persistence.entities;
 
-import com.bersyte.eventz.common.AppUser;
+import com.bersyte.eventz.common.UserEntity;
 import com.bersyte.eventz.features.event_participation.EventParticipation;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,21 +20,28 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "events")
+@EntityListeners(AuditingEntityListener.class)
 public class EventEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
     private String title;
     private String description;
     private String location;
     private LocalDateTime date;
     private Integer maxParticipants;
+    
+    @LastModifiedDate
     private LocalDateTime updateAt;
+    
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @ManyToOne
-    private AppUser organizer;
+    private UserEntity organizer;
 
-    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "event")
     private List<EventParticipation> participants;
+    
+    Integer participantsCount;
 }
