a829cd7ae5d8f8382c01cca1fbbbf0c82ac11f0d
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-29 16:07:11 +0200

    refactoring public and admin or organizers endpoints

---
+++ src/main/java/com/bersyte/eventz/features/events/presentation/controllers/OrganizerEventController.java
@@ -0,0 +1,84 @@
+package com.bersyte.eventz.features.events.presentation.controllers;
+
+import com.bersyte.eventz.common.domain.PagedResult;
+import com.bersyte.eventz.common.domain.Pagination;
+import com.bersyte.eventz.features.events.application.dtos.*;
+import com.bersyte.eventz.features.events.application.usecases.*;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1/organizers/events")
+@PreAuthorize("hasAnyRole('ORGANIZER', 'ADMIN')")
+public class OrganizerEventController {
+    
+    private final CreateEventUseCase createEventUseCase;
+    private final UpdateEventUseCase updateEventUseCase;
+    private final DeleteEventUseCase deleteEventUseCase;
+    private final FetchEventsByOrganizerUseCase fetchEventsByOrganizerUseCase;
+    
+    
+    public OrganizerEventController(CreateEventUseCase createEventUseCase, UpdateEventUseCase updateEventUseCase, DeleteEventUseCase deleteEventUseCase, FetchEventsByOrganizerUseCase fetchEventsByOrganizerUseCase) {
+        this.createEventUseCase = createEventUseCase;
+        this.updateEventUseCase = updateEventUseCase;
+        this.deleteEventUseCase = deleteEventUseCase;
+        this.fetchEventsByOrganizerUseCase = fetchEventsByOrganizerUseCase;
+    }
+    
+    @PostMapping
+    public ResponseEntity<EventResponse> create(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @Valid @RequestBody CreateEventRequest request
+    ) {
+        CreateEventInput input = new CreateEventInput(
+                userDetails.getUsername(),request
+        );
+        EventResponse response = createEventUseCase.execute(input);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    
+    
+    @PutMapping("{id}")
+    public ResponseEntity<EventResponse> updateEvent(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @PathVariable String id,
+            @Valid @RequestBody UpdateEventRequest request
+    ) {
+        UpdateEventInput input = new UpdateEventInput(
+                request , id,  userDetails.getUsername()
+        );
+        EventResponse response = updateEventUseCase.execute(input);
+        return ResponseEntity.ok(response);
+    }
+    
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEvent(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @PathVariable String id
+    ) {
+        DeleteEventInput input = new DeleteEventInput(
+                id,  userDetails.getUsername()
+        );
+        deleteEventUseCase.execute(input);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @GetMapping("/organizer")
+    public ResponseEntity<PagedResult<EventResponse>> eventByOrganizer(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        Pagination pagination = new Pagination(page, size);
+        EventsByOrganizerInput input = new EventsByOrganizerInput(
+                pagination,  userDetails.getUsername()
+        );
+        PagedResult<EventResponse>  response = fetchEventsByOrganizerUseCase.execute(input);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/bersyte/eventz/features/events/presentation/controllers/PublicEventController.java
@@ -0,0 +1,97 @@
+package com.bersyte.eventz.features.events.presentation.controllers;
+
+import com.bersyte.eventz.common.domain.PagedResult;
+import com.bersyte.eventz.common.domain.Pagination;
+import com.bersyte.eventz.features.events.application.dtos.*;
+import com.bersyte.eventz.features.events.application.usecases.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+
+@RestController
+@RequestMapping("/v1/events")
+public class PublicEventController {
+    
+    private final GetEventsByDateUseCase getEventsByDateUseCase;
+    private final GetEventByIdUseCase getEventByIdUseCase;
+    private final FetchEventsUseCase fetchEventsUseCase;
+    private final FetchUpcomingEventsUseCase fetchUpcomingEventsUseCase;
+    private final FilterEventsUseCase filterEventsUseCase;
+    
+
+    
+    public PublicEventController(
+                           GetEventsByDateUseCase getEventsByDateUseCase,
+                           GetEventByIdUseCase getEventByIdUseCase,
+                           FetchEventsUseCase fetchEventsUseCase,
+                           FetchUpcomingEventsUseCase fetchUpcomingEventsUseCase,
+                           FilterEventsUseCase filterEventsUseCase
+    ){
+        this.getEventsByDateUseCase = getEventsByDateUseCase;
+        this.getEventByIdUseCase = getEventByIdUseCase;
+        this.fetchEventsUseCase = fetchEventsUseCase;
+        this.fetchUpcomingEventsUseCase = fetchUpcomingEventsUseCase;
+        this.filterEventsUseCase = filterEventsUseCase;
+    }
+
+   
+   @GetMapping
+   public ResponseEntity<PagedResult<EventResponse>> getEvents(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        Pagination pagination = new Pagination(page, size);
+        PagedResult<EventResponse> events = fetchEventsUseCase.execute(pagination);
+        return ResponseEntity.ok(events);
+   }
+
+   @GetMapping("{id}")
+   public ResponseEntity<EventResponse> getEventById(@PathVariable String id) {
+       EventByIdInput input = new EventByIdInput("user-email-private-event", id);
+       EventResponse response = getEventByIdUseCase.execute(input);
+       return ResponseEntity.status(HttpStatus.OK).body(response);
+   }
+   
+
+   @GetMapping("/filter")
+   public ResponseEntity< PagedResult<EventResponse>> filterEvents(
+     @RequestParam(defaultValue = "0") int page,
+     @RequestParam(defaultValue = "10") int size,
+     @RequestParam(required = false) String title,
+     @RequestParam(required = false) String location,
+     @RequestParam LocalDateTime startDate,
+     @RequestParam LocalDateTime endDate
+   ) {
+       Pagination pagination = new Pagination(page, size);
+       EventSearchFilter filter = new EventSearchFilter(title, location,startDate, endDate ,pagination);
+       PagedResult<EventResponse> events = filterEventsUseCase.execute (filter);
+     return ResponseEntity.ok(events);
+   }
+
+    @GetMapping("/date")
+    public ResponseEntity< PagedResult<EventResponse>> getEventsByDate(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam LocalDateTime startDate,
+            @RequestParam LocalDateTime endDate
+    ) {
+        Pagination pagination = new Pagination(page, size);
+        EventByDateInput input = new EventByDateInput(pagination,startDate, endDate);
+        PagedResult<EventResponse> events = getEventsByDateUseCase.execute(input);
+        return ResponseEntity.ok(events);
+    }
+
+    @GetMapping("/upcoming")
+    public ResponseEntity< PagedResult<EventResponse>> getUpcomingEvents(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pagination pagination = new Pagination(page, size);
+        PagedResult<EventResponse>  upcomingEvents =
+                fetchUpcomingEventsUseCase.execute(pagination);
+        return ResponseEntity.ok(upcomingEvents);
+    }
+}
