212d5de26f64fb05687cdbadc3842ca2bebc295f
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-30 13:44:55 +0200

    refactoring: renaming method

--- src/main/java/com/bersyte/eventz/features/events/presentation/controllers/PublicEventController.java
+++ src/main/java/com/bersyte/eventz/features/events/presentation/controllers/PublicEventController.java
@@ -39,7 +39,7 @@ public class PublicEventController {
 
    
    @GetMapping
-   public ResponseEntity<PagedResult<EventResponse>> getEvents(
+   public ResponseEntity<PagedResult<EventResponse>> fetchEvents(
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "10") int size
     ) {
