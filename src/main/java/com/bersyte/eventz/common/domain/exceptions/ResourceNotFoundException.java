ed69661dd17ce9ae13be3c80a6de2a4535c2fc3c
IsaiasCuvula <bersyteinfo@gmail.com>
2026-01-31 08:21:59 +0200

    refactoring

--- src/main/java/com/bersyte/eventz/common/domain/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/bersyte/eventz/common/domain/exceptions/ResourceNotFoundException.java
@@ -2,6 +2,6 @@ package com.bersyte.eventz.common.domain.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String resourceName, String id) {
-        super(String.format("%s with id: %s not found", resourceName, id));
+        super(String.format("%s with Id: %s not found", resourceName, id));
     }
 }
