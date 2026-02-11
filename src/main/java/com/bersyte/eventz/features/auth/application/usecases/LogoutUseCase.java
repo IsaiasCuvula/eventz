4edcabd7fe0eb5c33ca1577b4d9856e2b48a01f5
IsaiasCuvula <bersyteinfo@gmail.com>
2026-02-11 08:37:56 +0200

    feat: logout use case

--- src/main/java/com/bersyte/eventz/features/auth/application/usecases/LogoutUseCase.java
+++ src/main/java/com/bersyte/eventz/features/auth/application/usecases/LogoutUseCase.java
@@ -1,4 +1,21 @@
 package com.bersyte.eventz.features.auth.application.usecases;
 
-public class LogoutUseCase {
+import com.bersyte.eventz.common.application.usecases.VoidUseCase;
+import com.bersyte.eventz.features.auth.domain.repository.RefreshTokenRepository;
+import com.bersyte.eventz.features.auth.domain.service.TokenService;
+
+public class LogoutUseCase implements VoidUseCase<String> {
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenService tokenService;
+    
+    public LogoutUseCase(RefreshTokenRepository refreshTokenRepository, TokenService tokenService) {
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.tokenService = tokenService;
+    }
+    
+    @Override
+    public void execute(String refreshToken) {
+        String tokenId = tokenService.extractTokenId(refreshToken);
+        refreshTokenRepository.deleteByTokenId(tokenId);
+    }
 }
